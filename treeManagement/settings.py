48229e315d409159b948e4429ae8c8b60753a514
root <root@vm-410102de-ea.novalocal>
2025-05-13 07:51:50 +0900

    vps change

--- treeManagement/settings.py
+++ treeManagement/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ev!&dhjh)o!ax-ky+(u1mtbm@x6^=-ou4462u2zpc0(ui)(%!+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['160.251.202.134', 'kasaipeach.jp'] 
 
 
 # Application definition
